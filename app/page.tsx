1e610be7d4a3975723f2396452d19bab883241ae
najafsikander <najafsikander@yahoo.com>
2024-08-21 17:04:08 +0400

    added posts card, interfaces & posts data

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import { IDashboardCardProps } from "@/interfaces/Dashboard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
+
+const dashboardCardData:Array<IDashboardCardProps> = [
+  {
+    title: 'Posts',
+    count: 100,
+    icon: <Newspaper className='text-slate-500' size={72} />
+  },
+  {
+    title: 'Categories',
+    count: 12,
+    icon: <Folder className='text-slate-500' size={72} />
+  },
+  {
+    title: 'Users',
+    count: 750,
+    icon: <User className='text-slate-500' size={72} />
+  },
+  {
+    title: 'Comments',
+    count: 1200,
+    icon: <MessageCircle className='text-slate-500' size={72} />
+  }
+]
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-2xl">Dashboard</h1>
+    <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
+      {dashboardCardData.map((dashboardCard:IDashboardCardProps) => (<DashboardCard key={dashboardCard.title} title={dashboardCard.title} count={dashboardCard.count} icon={dashboardCard.icon}/>))}
+    </div>
     </>
   );
 }
---
+++ interfaces/Dashboard/index.ts
@@ -0,0 +1,7 @@
+import { LucideIcon } from "lucide-react";
+
+export interface IDashboardCardProps {
+    title:string;
+    count:number;
+    icon: React.ReactElement<LucideIcon>
+}
